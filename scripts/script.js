56c4d5bc0481972047ee30992a873744102ccb6e
panhead63 <87744145+panhead63@users.noreply.github.com>
2023-09-11 20:46:47 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,28 @@
-$( document ).ready(function() {
-   
-  $('#dogMeme').click( 
-    function(
-    ){
-      //code goes here
-      console.log("position: ", $('#dogMeme').position() );
-    }
-  ); 
-  
- 
-  $( "#dogMeme" ).draggable({
-      stop: function() {
-        if(  $('#dogMeme').position().left > 500){
-        //  alert("you haswss wowzazzz ");
-          
-           $('#dogMeme').attr('src','https://static.demilked.com/wp-content/uploads/2022/09/it-humor-memes-5.jpeg');
-    }
-     
-        else if( $('#dogMeme').position().left > 300 && $('#dogMeme').position().left < 500){
-          alert("please try again");
-        }    
-        else {
-          alert("Awesome! You won! You have wow!");
-        }
-    }
-    
-    
-  
-    
-    
+$(document).ready(function () {
+  $("#dogMeme").click(function () {
+    //code goes here
+    console.log("position: ", $("#dogMeme").position());
   });
-  
-  
-  
-  
-  
+
+  $("#dogMeme").draggable({
+    stop: function () {
+      if ($("#dogMeme").position().left > 500) {
+        //  alert("you haswss wowzazzz ");
+
+        $("#dogeMemePic").attr(
+          "src",
+          "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Ff8%2Fde%2Fef%2Ff8deef878867dee11b7a15a0a55765b4.jpg&f=1&nofb=1&ipt=c9afc6feaa1e6f9483bb092ecc82257bd99612f623e1b1ef355cb04a4b31fd50&ipo=images"
+        );
+      } else if (
+        $("#dogMeme").position().left > 300 &&
+        $("#dogMeme").position().left < 500
+      ) {
+        //alert("please try again  ");
+        $("#gameOutput").text("please try again");
+      } else {
+        // alert("Congrats! You won! ");
+        $("#gameOutput").text("Congrats! You won! ");
+      }
+    },
+  });
 });
